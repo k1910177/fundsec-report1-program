f951500229d8433c0c473643f8535267f78798a8
k1910177 <riokanehiro@uec.ac.jp>
2024-04-28 18:07:35 +0900

    Export as PDF

--- benchmark.sh
+++ benchmark.sh
@@ -1,11 +1,11 @@
 mkdir -p result
 
 go test -bench=BenchmarkAES128SBox . -benchtime=10000000x -cpu=1 -memprofile=./result/mem128sbox.out -cpuprofile=./result/cpu128sbox.out -o=result/pprof128sbox.bin
-go tool pprof --png result/pprof128sbox.bin result/mem128sbox.out > result/mem128sbox.png
-go tool pprof --png result/pprof128sbox.bin result/cpu128sbox.out > result/cpu128sbox.png
+go tool pprof --pdf result/pprof128sbox.bin result/mem128sbox.out > result/mem128sbox.pdf
+go tool pprof --pdf result/pprof128sbox.bin result/cpu128sbox.out > result/cpu128sbox.pdf
 
 go test -bench=BenchmarkAES128TTable . -benchtime=10000000x -cpu=1 -memprofile=./result/mem128ttable.out -cpuprofile=./result/cpu128ttable.out -o=result/pprof128ttable.bin
-go tool pprof --png result/pprof128ttable.bin result/mem128ttable.out > result/mem128ttable.png
-go tool pprof --png result/pprof128ttable.bin result/cpu128ttable.out > result/cpu128ttable.png
+go tool pprof --pdf result/pprof128ttable.bin result/mem128ttable.out > result/mem128ttable.pdf
+go tool pprof --pdf result/pprof128ttable.bin result/cpu128ttable.out > result/cpu128ttable.pdf
 
 rm result/*.out result/*.bin
